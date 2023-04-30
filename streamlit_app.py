081d50aa52677c5c122391db5161e26142d8c235
Kwang-Il Son <113831194+sonyxkis@users.noreply.github.com>
2023-04-30 11:18:37 +0200

    Update streamlit_app.py
    Bug fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,4 @@ st.write('🥗 Omega 3 & Blueberry Oatmeal')
 st.text('🐔 Kale, Spanich & Rocket Smoothie')
 st.text('🥑🍞 Hard-Boiled Free-Range Egg')
    
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
