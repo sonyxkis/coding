be2145cd359aaed88fb2fd4571b0534cc7acce5e
Kwang-Il Son <113831194+sonyxkis@users.noreply.github.com>
2023-04-30 11:16:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,8 @@ import streamlit as st
 
 st.title('My Parents New Healthy Diner')
 
+st.header('Breakfast Menu')
+
 st.write('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spanich & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
