5a7ced97657a5c92918c15a0bc483db138c0a630
Kwang-Il Son <113831194+sonyxkis@users.noreply.github.com>
2023-04-30 23:42:55 +0200

    Update streamlit_app.py
    add : Let's Call the Fruityvice API from Our Streamlit App!

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 
 import streamlit as st
 import pandas as pd
+import requests
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+
+
 
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -21,3 +27,4 @@ fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 st.dataframe(fruits_to_show)
+st.text(fruityvice_response)
