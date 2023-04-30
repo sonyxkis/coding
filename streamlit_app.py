7aca6a5b442522c4a8066fd5b4278e77dec5e611
Kwang-Il Son <113831194+sonyxkis@users.noreply.github.com>
2023-04-30 23:46:28 +0200

    Update streamlit_app.py
    Let's Get the Fruityvice Data Looking a Little Nicer

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,8 @@ st.dataframe(fruits_to_show)
 st.header("Fruityvice Fruit Advice!")
 st.text(fruityvice_response.json())
 #st.text(fruityvice_response)
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+st.dataframe(fruityvice_normalized)
