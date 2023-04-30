766ef5bc0f75aac6fd109e82859a649eeb132e43
Kwang-Il Son <113831194+sonyxkis@users.noreply.github.com>
2023-04-30 12:52:58 +0200

    Update streamlit_app.py
    Add pandas read txt file

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 
-st.dataframe(my_fruit_list)
+
 
 st.title('My Parents New Healthy Diner')
 
@@ -15,3 +15,5 @@ st.text('🐔 Kale, Spanich & Rocket Smoothie')
 st.text('🥑🍞 Hard-Boiled Free-Range Egg')
    
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+st.dataframe(my_fruit_list)
