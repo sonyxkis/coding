f799db4bc43b2585df20d6fcf78fb38670372b92
Kwang-Il Son <113831194+sonyxkis@users.noreply.github.com>
2023-04-30 13:14:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 
 my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-fruits_to_show = my_fruit_list.set_index('Fruit')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 st.title('My Parents New Healthy Diner')
@@ -11,13 +11,13 @@ st.title('My Parents New Healthy Diner')
 st.header('🥣 Breakfast Menu')
 
 st.write('🥗 Omega 3 & Blueberry Oatmeal')
-st.text('🐔 Kale, Spanich & Rocket Smoothie')
-st.text('🥑🍞 Hard-Boiled Free-Range Egg')
+st.write('🐔 Kale, Spanich & Rocket Smoothie')
+st.write('🥑🍞 Hard-Boiled Free-Range Egg')
    
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruist_selected]
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 st.dataframe(my_fruit_list)
