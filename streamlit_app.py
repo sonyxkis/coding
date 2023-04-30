16a7f3b8a42551072393cf17b1c9398bc48dd013
Kwang-Il Son <113831194+sonyxkis@users.noreply.github.com>
2023-04-30 23:45:18 +0200

    Update streamlit_app.py
    Add st.text(fruityvice_response.json())

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,4 +27,7 @@ fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 st.dataframe(fruits_to_show)
-st.text(fruityvice_response)
+
+st.header("Fruityvice Fruit Advice!")
+st.text(fruityvice_response.json())
+#st.text(fruityvice_response)
