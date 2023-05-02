7fe5ee62fb1a88320c177ba488f68d59532dafb0
Kwang-Il Son <113831194+sonyxkis@users.noreply.github.com>
2023-05-02 22:08:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,17 +28,25 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+
 st.dataframe(fruits_to_show)
 
+
+#create the repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+   fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+   return fruityvice_normalized
+
 st.header("Fruityvice Fruit Advice!")
+
 try:
    fruit_choice = st.text_input('What fruit would you like information about?')
    if not fruit_choice:
       st.error('Please select a fruit to get information.')
    else:
       st.write('The user entered ', fruit_choice)
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+      back_from_function = get_fruityvice_data(fruit_choice)
       st.dataframe(fruityvice_normalized)
 except URLErrpr as e:
    st.error()
